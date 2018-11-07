40bfef1693427eb660645e518c32c04cc297c912
Aleksei Vozhdaev <soulburglar@gmail.com>
2018-11-07 16:01:58 +0300

    working animation

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -4207,7 +4207,7 @@ exports = module.exports = __webpack_require__(/*! ../../node_modules/css-loader
 
 
 // module
-exports.push([module.i, ".cls-1{isolation:isolate;}\n.cls-2{fill:url(#radial-gradient);}\n.cls-3{fill:url(#radial-gradient-2);}\n.cls-4{fill:#fff;}\n.cls-5{fill:url(#radial-gradient-3);}\n.cls-6{fill:#edeef0;}\n.cls-7{fill:#07a9f0;}\n.cls-8{fill:url(#radial-gradient-14);}\n.cls-10,\n.cls-11,\n.cls-12,\n.cls-14,\n.cls-9{mix-blend-mode:multiply;}\n.cls-9{fill:url(#radial-gradient-4);}\n.cls-10,\n.cls-11,\n.cls-12{opacity:0.6;}\n.cls-10{fill:url(#radial-gradient-5);}\n.cls-11{fill:url(#radial-gradient-6);}\n.cls-12{fill:url(#radial-gradient-7);}\n.cls-13{fill:#8078bf;}\n.cls-14{fill:url(#radial-gradient-8);}\n.cls-15{fill:url(#radial-gradient-9);}\n.cls-16{fill:#d8d9dd;}\n.cls-17{fill:url(#radial-gradient-10);}\n.cls-18{fill:url(#radial-gradient-11);}\n.cls-19{fill:url(#radial-gradient-12);}\n.cls-20{fill:url(#radial-gradient-13);}\n.cls-21{fill:aqua;}\n.cls-22{fill:url(#radial-gradient-15);}\n.cls-23{fill:#00e1ff;}\n.cls-24{fill:url(#radial-gradient-16);}\n\n.cls-3:hover {\n  fill: #007bff;\n}\n\ntext {\n  text-anchor: middle;\n}\n\n.text-main-circle {\n  font-weight: bold;\n  font-size: 40px;\n}\n\n.text-circle1,\n.text-circle2,\n.text-circle3,\n.text-circle4,\n.text-circle5 {\n  font-weight: bold;\n}\n\n.main-anim {\n  stroke: #007bff;\n  stroke-dasharray:820;\n  stroke-dashoffset:820;\n  stroke-width:18;\n  fill:transparent;\n}\n\n.btn3-selector {\n  cursor: pointer;\n}\n\n.btn-hover {\n  fill: #20c997;\n}\n", ""]);
+exports.push([module.i, ".cls-1{isolation:isolate;}\n.cls-2{fill:url(#radial-gradient);}\n.cls-3{fill:url(#radial-gradient-2);}\n.cls-4{fill:#fff;}\n.cls-5{fill:url(#radial-gradient-3);}\n.cls-6{fill:#edeef0;}\n.cls-7{fill:#07a9f0;}\n.cls-8{fill:url(#radial-gradient-14);}\n.cls-10,\n.cls-11,\n.cls-12,\n.cls-14,\n.cls-9{mix-blend-mode:multiply;}\n.cls-9{fill:url(#radial-gradient-4);}\n.cls-10,\n.cls-11,\n.cls-12{opacity:0.6;}\n.cls-10{fill:url(#radial-gradient-5);}\n.cls-11{fill:url(#radial-gradient-6);}\n.cls-12{fill:url(#radial-gradient-7);}\n.cls-13{fill:#8078bf;}\n.cls-14{fill:url(#radial-gradient-8);}\n.cls-15{fill:url(#radial-gradient-9);}\n.cls-16{fill:#d8d9dd;}\n.cls-17{fill:url(#radial-gradient-10);}\n.cls-18{fill:url(#radial-gradient-11);}\n.cls-19{fill:url(#radial-gradient-12);}\n.cls-20{fill:url(#radial-gradient-13);}\n.cls-21{fill:aqua;}\n.cls-22{fill:url(#radial-gradient-15);}\n.cls-23{fill:#00e1ff;}\n.cls-24{fill:url(#radial-gradient-16);}\n\n.cls-3:hover {\n  fill: #007bff;\n}\n\ntext {\n  text-anchor: middle;\n}\n\n.text-main-circle {\n  font-weight: bold;\n  font-size: 40px;\n}\n\n.text-circle1,\n.text-circle2,\n.text-circle3,\n.text-circle4,\n.text-circle5 {\n  font-weight: bold;\n}\n\n.main-anim {\n  stroke: #007bff;\n  stroke-dasharray:820;\n  stroke-dashoffset:820;\n  stroke-width:18;\n  fill:transparent;\n}\n\n.btn1-selector,\n.btn2-selector,\n.btn3-selector,\n.btn4-selector,\n.btn5-selector {\n  cursor: pointer;\n}\n\n.btn-hover {\n  fill: #20c997;\n}\n", ""]);
 
 // exports
 
@@ -43215,10 +43215,16 @@ var Section3 = function (_React$Component) {
   _createClass(Section3, [{
     key: 'componentDidMount',
     value: function componentDidMount() {
-      (0, _jquery2.default)(".btn3-selector").hover(function () {
-        (0, _jquery2.default)(".btn3").addClass("btn-hover");
-      }, function () {
-        (0, _jquery2.default)(".btn3").removeClass("btn-hover");
+      var selectors = [".btn1-selector", ".btn2-selector", ".btn3-selector", ".btn4-selector", ".btn5-selector"];
+
+      selectors.map(function (selector) {
+        (0, _jquery2.default)(selector).hover(function (event) {
+          var select = '.' + getSelector(event);
+          (0, _jquery2.default)(select).addClass("btn-hover");
+        }, function (event) {
+          var select = '.' + getSelector(event);
+          (0, _jquery2.default)(select).removeClass("btn-hover");
+        });
       });
     }
   }, {
@@ -43306,25 +43312,13 @@ var Section3 = function (_React$Component) {
                 _react2.default.createElement('path', { className: 'cls-3', d: 'M550.76,397.87a152.15,152.15,0,0,0-38.34-101.19A152.71,152.71,0,0,0,287.59,503.32,152.71,152.71,0,0,0,550.76,397.87Z' }),
                 _react2.default.createElement('path', { className: 'cls-5', d: 'M515.12,398.38A116.61,116.61,0,1,0,398.51,515,116.13,116.13,0,0,0,515.12,398.38Z' }),
                 _react2.default.createElement('path', { className: 'cls-6', d: 'M400,217.54A182.46,182.46,0,1,1,271,271a181.27,181.27,0,0,1,129-53.44m0-8c-105.19,0-190.46,85.27-190.46,190.46S294.81,590.46,400,590.46,590.46,505.19,590.46,400,505.19,209.54,400,209.54Z' }),
-                _react2.default.createElement('rect', { className: 'cls-7 btn5', x: '586.8', y: '400.26', width: '76.91', height: '6.23' }),
                 _react2.default.createElement('path', { className: 'cls-8', d: 'M398.51,209.55v7A182.68,182.68,0,0,1,582.7,400.73h7A189.65,189.65,0,0,0,398.51,209.55Z' }),
                 _react2.default.createElement('path', { className: 'cls-9', d: 'M608.94,407.16h0l-12.57-12.54h0a13,13,0,1,0-18.08,18.56L590,424.86h0a13,13,0,1,0,19-17.7Z' }),
                 _react2.default.createElement('path', { className: 'cls-8', d: 'M209.38,399.44h7a182.69,182.69,0,0,1,182.9-182.9v-7a189.69,189.69,0,0,0-189.9,189.9Z' }),
                 _react2.default.createElement('path', { className: 'cls-10', d: 'M553.7,271.26h0l-12.56-12.54h0a13,13,0,1,0-18.08,18.56L534.73,289h0a13,13,0,1,0,19-17.71Z' }),
                 _react2.default.createElement('path', { className: 'cls-11', d: 'M421.43,217.2h0l-12.57-12.54h0a13,13,0,1,0-18.08,18.56l11.68,11.68h0a13,13,0,1,0,19-17.71Z' }),
                 _react2.default.createElement('path', { className: 'cls-12', d: 'M290.68,272.55h0L278.12,260h0A13,13,0,1,0,260,278.57l11.68,11.68h0a13,13,0,1,0,19-17.7Z' }),
-                _react2.default.createElement('rect', { className: 'cls-8 btn2', x: '238.22', y: '203.13', width: '6.23', height: '76.91', transform: 'translate(-100.14 241.41) rotate(-45)' }),
-                _react2.default.createElement('path', { className: 'cls-8 btn2', d: 'M259.35,278a13,13,0,1,1,18.37,0A13,13,0,0,1,259.35,278Z' }),
-                _react2.default.createElement('circle', { className: 'cls-7 btn5', cx: '586.8', cy: '403.38', r: '12.99', transform: 'translate(-110.18 257.57) rotate(-22.69)' }),
-                _react2.default.createElement('rect', { className: 'cls-8 btn4', x: '520.29', y: '237.18', width: '76.91', height: '6.24', transform: 'translate(-6.26 465.48) rotate(-45)' }),
-                _react2.default.createElement('path', { className: 'cls-8 btn4', d: 'M522.37,258.3a13,13,0,1,1,0,18.37A13,13,0,0,1,522.37,258.3Z' }),
                 _react2.default.createElement('path', { className: 'cls-14', d: 'M235.78,407.16h0l-12.56-12.54h0a13,13,0,1,0-18.09,18.56l11.69,11.68h0a13,13,0,1,0,19-17.7Z' }),
-                _react2.default.createElement('path', { className: 'cls-8 btn2', d: 'M260.63,186.83a73.51,73.51,0,0,0-18.53-48.9,73.79,73.79,0,0,0-108.63,99.85,73.79,73.79,0,0,0,127.16-50.95Z' }),
-                _react2.default.createElement('path', { className: 'cls-15', d: 'M243.41,187.07a56.34,56.34,0,1,0-56.35,56.35A56.13,56.13,0,0,0,243.41,187.07Z' }),
-                _react2.default.createElement('path', { className: 'cls-17 btn5', d: 'M773.79,399A73.79,73.79,0,1,0,700,472.76,73.48,73.48,0,0,0,773.79,399Z' }),
-                _react2.default.createElement('path', { className: 'cls-18', d: 'M756.57,399.22a56.34,56.34,0,1,0-56.34,56.34A56.09,56.09,0,0,0,756.57,399.22Z' }),
-                _react2.default.createElement('path', { className: 'cls-19 btn4', d: 'M685.24,186a73.5,73.5,0,0,0-18.53-48.89A73.78,73.78,0,1,0,685.24,186Z' }),
-                _react2.default.createElement('path', { className: 'cls-20', d: 'M668,186.29a56.34,56.34,0,1,0-56.35,56.34A56.14,56.14,0,0,0,668,186.29Z' }),
                 _react2.default.createElement(
                   'g',
                   { className: 'btn1-selector' },
@@ -43332,7 +43326,15 @@ var Section3 = function (_React$Component) {
                   _react2.default.createElement('path', { className: 'cls-23 btn1', d: 'M213.63,416.37a13,13,0,1,1,13-13A13,13,0,0,1,213.63,416.37Z' }),
                   _react2.default.createElement('path', { className: 'cls-23 btn1', d: 'M171.9,399a73.48,73.48,0,0,0-18.53-48.89A73.78,73.78,0,0,0,44.74,449.92,73.79,73.79,0,0,0,171.9,399Z' })
                 ),
-                _react2.default.createElement('path', { className: 'cls-24', d: 'M154.68,399.22a56.14,56.14,0,0,0-14.15-37.34,56.34,56.34,0,0,0-82.95,76.24,56.34,56.34,0,0,0,97.1-38.9Z' }),
+                _react2.default.createElement('path', { className: 'cls-24 btn1-selector', d: 'M154.68,399.22a56.14,56.14,0,0,0-14.15-37.34,56.34,56.34,0,0,0-82.95,76.24,56.34,56.34,0,0,0,97.1-38.9Z' }),
+                _react2.default.createElement(
+                  'g',
+                  { className: 'btn2-selector' },
+                  _react2.default.createElement('path', { className: 'cls-8 btn2', d: 'M260.63,186.83a73.51,73.51,0,0,0-18.53-48.9,73.79,73.79,0,0,0-108.63,99.85,73.79,73.79,0,0,0,127.16-50.95Z' }),
+                  _react2.default.createElement('rect', { className: 'cls-8 btn2', x: '238.22', y: '203.13', width: '6.23', height: '76.91', transform: 'translate(-100.14 241.41) rotate(-45)' }),
+                  _react2.default.createElement('path', { className: 'cls-8 btn2', d: 'M259.35,278a13,13,0,1,1,18.37,0A13,13,0,0,1,259.35,278Z' })
+                ),
+                _react2.default.createElement('path', { className: 'cls-15 btn2-selector', d: 'M243.41,187.07a56.34,56.34,0,1,0-56.35,56.35A56.13,56.13,0,0,0,243.41,187.07Z' }),
                 _react2.default.createElement(
                   'g',
                   { className: 'btn3-selector' },
@@ -43340,7 +43342,23 @@ var Section3 = function (_React$Component) {
                   _react2.default.createElement('path', { className: 'cls-21 btn3', d: 'M386.29,213.43a13,13,0,1,1,13,13A13,13,0,0,1,386.29,213.43Z' }),
                   _react2.default.createElement('path', { className: 'cls-21 btn3', d: 'M472.85,99.7a73.51,73.51,0,0,0-18.54-48.9,73.79,73.79,0,0,0-108.63,99.85A73.79,73.79,0,0,0,472.85,99.7Z' })
                 ),
-                _react2.default.createElement('path', { className: 'cls-22 btn3-selector', d: 'M455.62,99.94A56.13,56.13,0,0,0,441.47,62.6a56.35,56.35,0,0,0-83,76.25,56.34,56.34,0,0,0,97.1-38.91Z' })
+                _react2.default.createElement('path', { className: 'cls-22 btn3-selector', d: 'M455.62,99.94A56.13,56.13,0,0,0,441.47,62.6a56.35,56.35,0,0,0-83,76.25,56.34,56.34,0,0,0,97.1-38.91Z' }),
+                _react2.default.createElement(
+                  'g',
+                  { className: 'btn4-selector' },
+                  _react2.default.createElement('rect', { className: 'cls-8 btn4', x: '520.29', y: '237.18', width: '76.91', height: '6.24', transform: 'translate(-6.26 465.48) rotate(-45)' }),
+                  _react2.default.createElement('path', { className: 'cls-8 btn4', d: 'M522.37,258.3a13,13,0,1,1,0,18.37A13,13,0,0,1,522.37,258.3Z' }),
+                  _react2.default.createElement('path', { className: 'cls-19 btn4', d: 'M685.24,186a73.5,73.5,0,0,0-18.53-48.89A73.78,73.78,0,1,0,685.24,186Z' })
+                ),
+                _react2.default.createElement('path', { className: 'cls-20 btn4-selector', d: 'M668,186.29a56.34,56.34,0,1,0-56.35,56.34A56.14,56.14,0,0,0,668,186.29Z' }),
+                _react2.default.createElement(
+                  'g',
+                  { className: 'btn5-selector' },
+                  _react2.default.createElement('rect', { className: 'cls-7 btn5', x: '586.8', y: '400.26', width: '76.91', height: '6.23' }),
+                  _react2.default.createElement('path', { className: 'cls-17 btn5', d: 'M773.79,399A73.79,73.79,0,1,0,700,472.76,73.48,73.48,0,0,0,773.79,399Z' }),
+                  _react2.default.createElement('circle', { className: 'cls-7 btn5', cx: '586.8', cy: '403.38', r: '12.99', transform: 'translate(-110.18 257.57) rotate(-22.69)' })
+                ),
+                _react2.default.createElement('path', { className: 'cls-18 btn5-selector', d: 'M756.57,399.22a56.34,56.34,0,1,0-56.34,56.34A56.09,56.09,0,0,0,756.57,399.22Z' })
               ),
               _react2.default.createElement(
                 'text',
@@ -43354,32 +43372,32 @@ var Section3 = function (_React$Component) {
               ),
               _react2.default.createElement(
                 'text',
-                { className: 'text-circle1', x: '100', y: '390' },
+                { className: 'text-circle1 btn1-selector', x: '100', y: '390' },
                 '\u0422\u0435\u043E\u0440\u0438\u044F'
               ),
               _react2.default.createElement(
                 'text',
-                { className: 'text-circle1', x: '100', y: '410' },
+                { className: 'text-circle1 btn1-selector', x: '100', y: '410' },
                 '\u043F\u0440\u043E Instagram'
               ),
               _react2.default.createElement(
                 'text',
-                { className: 'text-circle2', x: '190', y: '190' },
+                { className: 'text-circle2 btn2-selector', x: '190', y: '190' },
                 '\u041A\u043E\u043D\u0442\u0435\u043D\u0442'
               ),
               _react2.default.createElement(
                 'text',
-                { className: 'text-circle3', x: '400', y: '105' },
+                { className: 'text-circle3 btn3-selector', x: '400', y: '105' },
                 '\u0424\u043E\u0442\u043E-\u043A\u043E\u043D\u0442\u0435\u043D\u0442'
               ),
               _react2.default.createElement(
                 'text',
-                { className: 'text-circle4', x: '612', y: '190' },
+                { className: 'text-circle4 btn4-selector', x: '612', y: '190' },
                 '\u041F\u0440\u043E\u0434\u0432\u0438\u0436\u0435\u043D\u0438\u0435'
               ),
               _react2.default.createElement(
                 'text',
-                { className: 'text-circle5', x: '700', y: '405' },
+                { className: 'text-circle5 btn5-selector', x: '700', y: '405' },
                 'Stories'
               )
             )
@@ -43392,24 +43410,17 @@ var Section3 = function (_React$Component) {
   return Section3;
 }(_react2.default.Component);
 
-/*            <Card containerKlass="card col-lg-4 col-md-6 col-12"
-                  imgpath="images/card1.jpg"
-                  cardKlass="card1"
-                  title="Новичкам"
-                  tekst="Курс поможет Вам разобраться: как работает Instagram; как Вы можете начать зарабатывать на ведении других страничек." />
-            <Card containerKlass="card col-lg-4 col-md-6 col-12"
-                  imgpath="images/card2.jpg"
-                  cardKlass="card2"
-                  title="Специалистам с базовыми знаниями"
-                  tekst="Полная проверка домашнего задания поможет Вам найти свои ошибки. Так же курс полезен тем, кто хочет узнать новейшие инструменты и изменения в Instagram." />
-            <Card containerKlass=" card col-lg-4 col-md-6 col-12"
-                  imgpath="images/card3.jpg"
-                  cardKlass="card3"
-                  title="Владельцам бизнеса и самозанятым"
-                  tekst="Курс поможет Вам самостоятельно развивать Вашу страничку. Если Вам важно понять, что 'заходит' в Instagram, а что нет." />*/
-
-
 exports.default = Section3;
+
+
+var getSelector = function getSelector(event) {
+  var classList = (0, _jquery2.default)(event.currentTarget).attr('class').split(/\s+/);
+  var preparedClassList = classList.filter(function (word) {
+    return word.split('-')[1] === 'selector';
+  });
+  var select = preparedClassList[0].split('-')[0];
+  return select;
+};
 
 /***/ }),
 
--- src/js/components/container/Section3.js
+++ src/js/components/container/Section3.js
@@ -3,13 +3,27 @@ import $ from 'jquery';
 
 export default class Section3 extends React.Component {
   componentDidMount() {
-    $( ".btn3-selector" ).hover(
-      function() {
-        $( ".btn3" ).addClass("btn-hover");
-      }, function() {
-        $( ".btn3" ).removeClass("btn-hover");
-      }
-    );
+    let selectors = [
+      ".btn1-selector",
+      ".btn2-selector",
+      ".btn3-selector",
+      ".btn4-selector",
+      ".btn5-selector",
+    ];
+
+    selectors.map((selector) => {
+      $( selector ).hover(
+        function(event) {
+          let select = `.${getSelector(event)}`;
+          $( select ).addClass("btn-hover");
+        }, function(event) {
+          let select = `.${getSelector(event)}`;
+          $( select ).removeClass("btn-hover");
+        }
+      );
+    });
+
+
   }
 
   render() {
@@ -69,33 +83,27 @@ export default class Section3 extends React.Component {
                 <path className="cls-3" d="M550.76,397.87a152.15,152.15,0,0,0-38.34-101.19A152.71,152.71,0,0,0,287.59,503.32,152.71,152.71,0,0,0,550.76,397.87Z"/>
                 <path className="cls-5" d="M515.12,398.38A116.61,116.61,0,1,0,398.51,515,116.13,116.13,0,0,0,515.12,398.38Z"/>
                 <path className="cls-6" d="M400,217.54A182.46,182.46,0,1,1,271,271a181.27,181.27,0,0,1,129-53.44m0-8c-105.19,0-190.46,85.27-190.46,190.46S294.81,590.46,400,590.46,590.46,505.19,590.46,400,505.19,209.54,400,209.54Z"/>
-                <rect className="cls-7 btn5" x="586.8" y="400.26" width="76.91" height="6.23"/>
                 <path className="cls-8" d="M398.51,209.55v7A182.68,182.68,0,0,1,582.7,400.73h7A189.65,189.65,0,0,0,398.51,209.55Z"/>
                 <path className="cls-9" d="M608.94,407.16h0l-12.57-12.54h0a13,13,0,1,0-18.08,18.56L590,424.86h0a13,13,0,1,0,19-17.7Z"/>
                 <path className="cls-8" d="M209.38,399.44h7a182.69,182.69,0,0,1,182.9-182.9v-7a189.69,189.69,0,0,0-189.9,189.9Z"/>
                 <path className="cls-10" d="M553.7,271.26h0l-12.56-12.54h0a13,13,0,1,0-18.08,18.56L534.73,289h0a13,13,0,1,0,19-17.71Z"/>
                 <path className="cls-11" d="M421.43,217.2h0l-12.57-12.54h0a13,13,0,1,0-18.08,18.56l11.68,11.68h0a13,13,0,1,0,19-17.71Z"/>
                 <path className="cls-12" d="M290.68,272.55h0L278.12,260h0A13,13,0,1,0,260,278.57l11.68,11.68h0a13,13,0,1,0,19-17.7Z"/>
-                <rect className="cls-8 btn2" x="238.22" y="203.13" width="6.23" height="76.91" transform="translate(-100.14 241.41) rotate(-45)"/>
-                <path className="cls-8 btn2" d="M259.35,278a13,13,0,1,1,18.37,0A13,13,0,0,1,259.35,278Z"/>
-                <circle className="cls-7 btn5" cx="586.8" cy="403.38" r="12.99" transform="translate(-110.18 257.57) rotate(-22.69)"/>
-                <rect className="cls-8 btn4" x="520.29" y="237.18" width="76.91" height="6.24" transform="translate(-6.26 465.48) rotate(-45)"/>
-                <path className="cls-8 btn4" d="M522.37,258.3a13,13,0,1,1,0,18.37A13,13,0,0,1,522.37,258.3Z"/>
                 <path className="cls-14" d="M235.78,407.16h0l-12.56-12.54h0a13,13,0,1,0-18.09,18.56l11.69,11.68h0a13,13,0,1,0,19-17.7Z"/>
-
-                <path className="cls-8 btn2" d="M260.63,186.83a73.51,73.51,0,0,0-18.53-48.9,73.79,73.79,0,0,0-108.63,99.85,73.79,73.79,0,0,0,127.16-50.95Z"/>
-                <path className="cls-15" d="M243.41,187.07a56.34,56.34,0,1,0-56.35,56.35A56.13,56.13,0,0,0,243.41,187.07Z"/>
-                <path className="cls-17 btn5" d="M773.79,399A73.79,73.79,0,1,0,700,472.76,73.48,73.48,0,0,0,773.79,399Z"/>
-                <path className="cls-18" d="M756.57,399.22a56.34,56.34,0,1,0-56.34,56.34A56.09,56.09,0,0,0,756.57,399.22Z"/>
-                <path className="cls-19 btn4" d="M685.24,186a73.5,73.5,0,0,0-18.53-48.89A73.78,73.78,0,1,0,685.24,186Z"/>
-                <path className="cls-20" d="M668,186.29a56.34,56.34,0,1,0-56.35,56.34A56.14,56.14,0,0,0,668,186.29Z"/>
 
                 <g className="btn1-selector">
                   <rect className="cls-23 btn1" x="136.73" y="400.26" width="76.91" height="6.24"/>
                   <path className="cls-23 btn1" d="M213.63,416.37a13,13,0,1,1,13-13A13,13,0,0,1,213.63,416.37Z"/>
                   <path className="cls-23 btn1" d="M171.9,399a73.48,73.48,0,0,0-18.53-48.89A73.78,73.78,0,0,0,44.74,449.92,73.79,73.79,0,0,0,171.9,399Z"/>
                 </g>
-                <path className="cls-24" d="M154.68,399.22a56.14,56.14,0,0,0-14.15-37.34,56.34,56.34,0,0,0-82.95,76.24,56.34,56.34,0,0,0,97.1-38.9Z"/>
+                <path className="cls-24 btn1-selector" d="M154.68,399.22a56.14,56.14,0,0,0-14.15-37.34,56.34,56.34,0,0,0-82.95,76.24,56.34,56.34,0,0,0,97.1-38.9Z"/>
+
+                <g className="btn2-selector">
+                  <path className="cls-8 btn2" d="M260.63,186.83a73.51,73.51,0,0,0-18.53-48.9,73.79,73.79,0,0,0-108.63,99.85,73.79,73.79,0,0,0,127.16-50.95Z"/>
+                  <rect className="cls-8 btn2" x="238.22" y="203.13" width="6.23" height="76.91" transform="translate(-100.14 241.41) rotate(-45)"/>
+                  <path className="cls-8 btn2" d="M259.35,278a13,13,0,1,1,18.37,0A13,13,0,0,1,259.35,278Z"/>
+                </g>
+                <path className="cls-15 btn2-selector" d="M243.41,187.07a56.34,56.34,0,1,0-56.35,56.35A56.13,56.13,0,0,0,243.41,187.07Z"/>
 
                 <g className="btn3-selector">
                   <rect className="cls-21 btn3" x="396.16" y="136.52" width="6.24" height="76.91"/>
@@ -104,6 +112,19 @@ export default class Section3 extends React.Component {
                 </g>
                 <path className="cls-22 btn3-selector" d="M455.62,99.94A56.13,56.13,0,0,0,441.47,62.6a56.35,56.35,0,0,0-83,76.25,56.34,56.34,0,0,0,97.1-38.91Z"/>
 
+                <g className="btn4-selector">
+                  <rect className="cls-8 btn4" x="520.29" y="237.18" width="76.91" height="6.24" transform="translate(-6.26 465.48) rotate(-45)"/>
+                  <path className="cls-8 btn4" d="M522.37,258.3a13,13,0,1,1,0,18.37A13,13,0,0,1,522.37,258.3Z"/>
+                  <path className="cls-19 btn4" d="M685.24,186a73.5,73.5,0,0,0-18.53-48.89A73.78,73.78,0,1,0,685.24,186Z"/>
+                </g>
+                <path className="cls-20 btn4-selector" d="M668,186.29a56.34,56.34,0,1,0-56.35,56.34A56.14,56.14,0,0,0,668,186.29Z"/>
+
+                <g className="btn5-selector">
+                  <rect className="cls-7 btn5" x="586.8" y="400.26" width="76.91" height="6.23"/>
+                  <path className="cls-17 btn5" d="M773.79,399A73.79,73.79,0,1,0,700,472.76,73.48,73.48,0,0,0,773.79,399Z"/>
+                  <circle className="cls-7 btn5" cx="586.8" cy="403.38" r="12.99" transform="translate(-110.18 257.57) rotate(-22.69)"/>
+                </g>
+                <path className="cls-18 btn5-selector" d="M756.57,399.22a56.34,56.34,0,1,0-56.34,56.34A56.09,56.09,0,0,0,756.57,399.22Z"/>
 
               </g>
               <text className="text-main-circle" x="400" y="380">
@@ -112,22 +133,22 @@ export default class Section3 extends React.Component {
               <text className="text-main-circle" x="400" y="420">
                 на курсе?
               </text>
-              <text className="text-circle1" x="100" y="390">
+              <text className="text-circle1 btn1-selector" x="100" y="390">
                 Теория
               </text>
-              <text className="text-circle1" x="100" y="410">
+              <text className="text-circle1 btn1-selector" x="100" y="410">
                 про Instagram
               </text>
-              <text className="text-circle2" x="190" y="190">
+              <text className="text-circle2 btn2-selector" x="190" y="190">
                 Контент
               </text>
-              <text className="text-circle3" x="400" y="105">
+              <text className="text-circle3 btn3-selector" x="400" y="105">
                 Фото-контент
               </text>
-              <text className="text-circle4" x="612" y="190">
+              <text className="text-circle4 btn4-selector" x="612" y="190">
                 Продвижение
               </text>
-              <text className="text-circle5" x="700" y="405">
+              <text className="text-circle5 btn5-selector" x="700" y="405">
                 Stories
               </text>
             </g>
@@ -138,18 +159,9 @@ export default class Section3 extends React.Component {
   }
 }
 
-/*            <Card containerKlass="card col-lg-4 col-md-6 col-12"
-                  imgpath="images/card1.jpg"
-                  cardKlass="card1"
-                  title="Новичкам"
-                  tekst="Курс поможет Вам разобраться: как работает Instagram; как Вы можете начать зарабатывать на ведении других страничек." />
-            <Card containerKlass="card col-lg-4 col-md-6 col-12"
-                  imgpath="images/card2.jpg"
-                  cardKlass="card2"
-                  title="Специалистам с базовыми знаниями"
-                  tekst="Полная проверка домашнего задания поможет Вам найти свои ошибки. Так же курс полезен тем, кто хочет узнать новейшие инструменты и изменения в Instagram." />
-            <Card containerKlass=" card col-lg-4 col-md-6 col-12"
-                  imgpath="images/card3.jpg"
-                  cardKlass="card3"
-                  title="Владельцам бизнеса и самозанятым"
-                  tekst="Курс поможет Вам самостоятельно развивать Вашу страничку. Если Вам важно понять, что 'заходит' в Instagram, а что нет." />*/
+const getSelector = (event) => {
+  let classList = $(event.currentTarget).attr('class').split(/\s+/);
+  let preparedClassList = classList.filter(word => word.split('-')[1] === 'selector');
+  let select = preparedClassList[0].split('-')[0];
+  return select;
+};
