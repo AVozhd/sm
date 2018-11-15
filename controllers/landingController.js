bd68ac9e0e5d4733f3efe119165a190e14f4030d
Aleksei Vozhdaev <soulburglar@gmail.com>
2018-11-15 03:24:25 +0300

    capcha init 50%

--- controllers/landingController.js
+++ controllers/landingController.js
@@ -1,4 +1,36 @@
+var request = require("request");
+
 exports.index = function(req, res) {
+  if (
+    req.body.captcha === undefined ||
+    req.body.captcha === "" ||
+    req.body.captcha === null
+  ) {
+    return res.json({ success: false, message: "Please select captcha" });
+  }
+
+  var secretKey = "6LfavnoUAAAAAN82ww24_5OD2NkPRXm9zUYpSzmJ";
+  var verifyUrl =
+    "https://www.google.com/recaptcha/api/siteverify?secret=" +
+    secretKey +
+    "&response=" +
+    req.body.captcha +
+    "&remoteip=" +
+    req.connection.remoteAddress;
+
+  request(verifyUrl, (err, response, body) => {
+    body = JSON.parse(body);
+    if (body.success !== undefined && !body.success) {
+      return res.json({
+        success: false,
+        message: "Failed captcha verification"
+      });
+    } else {
+      res.render("landing", { title: "Self Marketing" });
+      // return res.json({ success: true, message: "Captcha passed" });
+    }
+  });
+
   res.render("landing", { title: "Self Marketing" });
   // async.parallel({}, function(err, results) {
   //     res.render('landing', { title: 'Self Marketing', error: err });
